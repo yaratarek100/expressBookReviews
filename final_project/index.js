01e8a42896812bf40e9a102ef8f691a9a102218e
Yara-Tarek <135819372+Yara-Tarek@users.noreply.github.com>
2023-10-06 04:44:40 +0200

    commit

--- final_project/index.js
+++ final_project/index.js
@@ -4,19 +4,56 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
+
+
+
+
+
 const app = express();
 
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+// app.use(session({secret:"fingerpint"}))
+// app.use(express.json());
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (req.session.authorization) {
+      const token = req.session.authorization.accessToken; // تحقق من وجود رمز المصادقة
+      jwt.verify(token, "access", (err, user) => {
+        if (!err) {
+          req.user = user;
+          next();
+        } else {
+          return res.status(403).json({message: "User not authenticated"});
+        }
+      });
+    } else {
+      return res.status(403).json({message: "User not logged in"});
+    }
+  });
+
+// app.use("/customer/auth/*", function auth(req,res,next){
+    
+//        if(req.session.authorization) {
+//            token = req.session.authorization['accessToken'];
+//            jwt.verify(token, "access",(err,user)=>{
+//                if(!err){
+//                    req.user = user;
+//                    next();
+//                }
+//                else{
+//                    return res.status(403).json({message: "User not authenticated"})
+//                }
+//             });
+//         } else {
+//             return res.status(403).json({message: "User not logged in"})
+//         }
+//     });
  
 const PORT =5000;
 
+
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
-
 app.listen(PORT,()=>console.log("Server is running"));
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,70 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!isValid(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});    
+    }
+  } 
+  return res.status(404).json({message: "Unable to register user."});
+
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const isbn = req.params.isbn;
+  res.send(books[isbn])
+// console.log(isbn);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  // console.log(author);
+  
+  let bookArray = Object.values(books);
+    let sbook = bookArray.filter((book) => book.author === author);
+    res.send(sbook);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const title = req.params.title;
+  // console.log(author);
+  
+  let bookArray = Object.values(books);
+    let sbook = bookArray.filter((book) => book.title === title);
+    res.send(sbook);
 });
 
+public_users.delete("/:book", (req, res) => {
+  const email = req.params.email;
+  if (email){
+      delete books[email]
+  }
+  res.send(`book with the email  ${email} deleted.`);
+});
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn].reviews)
 });
 
 module.exports.general = public_users;
